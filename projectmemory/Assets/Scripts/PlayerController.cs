0ff0a55bbc42e16015be5f40adac35085e34ffae
general124@hotmail.com <>
2025-10-26 15:54:03 +1300

    added initial character sprites, small spacing changes

--- projectmemory/Assets/Scripts/PlayerController.cs
+++ projectmemory/Assets/Scripts/PlayerController.cs
@@ -21,9 +21,9 @@ public class PlayerController : MonoBehaviour
     {
         NONE    = 0,
         UP      = 1 << 1,
-        DOWN = 1 << 2,
-        LEFT  = 1 << 3,
-        RIGHT = 1 << 4,
+        DOWN    = 1 << 2,
+        LEFT    = 1 << 3,
+        RIGHT   = 1 << 4,
     }
 
     public direction worldViewLook = 0;
