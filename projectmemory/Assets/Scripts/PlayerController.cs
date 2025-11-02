e36c21f214dac29bd027c076e3e3a8194b7665b0
general124@hotmail.com <>
2025-11-02 15:08:07 +1300

    interact icon implemented
    -pops up when near and interatable
    -q and e (prev and next actions) move the icon between nearby interactables
    -moves to next interactable if player moves away from one.
    -disappears when player moves away from interactables (attaches to player object and is disabled)

--- projectmemory/Assets/Scripts/PlayerController.cs
+++ projectmemory/Assets/Scripts/PlayerController.cs
@@ -30,6 +30,7 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private Rigidbody2D playerRB;
     [SerializeField] private float moveSpeed = 10.0f;
+    [SerializeField] private GameObject interactIconObject;
 
     private InputActionMap playerControlMap;
     private Dictionary<int, GameObject> nearbyInteractables = new Dictionary<int, GameObject>();
@@ -37,7 +38,7 @@ public class PlayerController : MonoBehaviour
     private int interactableIndex = -1;
 
     private Vector2 inputMove = Vector2.zero;
- 
+
     InputAction moveAction;
     InputAction interactAction;
     InputAction prevAction;
@@ -45,17 +46,20 @@ public class PlayerController : MonoBehaviour
 
     private void OnEnable()
     {
-        playerControlMap.Enable();
+        if (playerControlMap != null)
+        {
+            playerControlMap.Enable();
+        }
         //MARK: update as gamemaster is built
         //Refreash skill/statuses/stats/equipped stuff
-        moveSpeed = GameMaster._instance.player_moveSpeed;    
+        moveSpeed = GameMaster._instance.player_moveSpeed;
     }
 
 
     private void OnDisable()
-    {   
+    {
         //disable controls when player controller not longer active        
-       playerControlMap.Disable();
+        playerControlMap.Disable();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -66,6 +70,12 @@ public class PlayerController : MonoBehaviour
         playerControlMap = InputSystem.actions.FindActionMap("Player");
         prevAction = InputSystem.actions.FindAction("Previous");
         nextAction = InputSystem.actions.FindAction("Next");
+
+        if (interactIconObject == null)
+        {
+            Debug.LogWarning("[PlayerController]: interact icon object not set.");
+        }
+        HighlightNearbyInteractable(false);
     }
 
     // Update is called once per frame
@@ -78,21 +88,33 @@ public class PlayerController : MonoBehaviour
             //activate interact on selected interactable
             Debug.Log($"[PlayerController]: Attempting to interact");
         }
-        else if(nearbyInteractables.Count > 0)
+        else if (nearbyInteractables.Count > 0)
         {
-            if(prevAction.WasPerformedThisFrame())
+            if (prevAction.WasPerformedThisFrame())
             {
                 Debug.Log($"[PlayerController]: Switch target to previous nearby interactable");
+                interactableIndex--;
+                if (interactableIndex < 0)
+                {
+                    interactableIndex = nearbyInteractables.Count - 1;
+                }
+                HighlightNearbyInteractable();
             }
-            else if(nextAction.WasPressedThisFrame())
+            else if (nextAction.WasPressedThisFrame())
             {
                 Debug.Log($"[PlayerController]: Switch target to next nearby interactable");
+                interactableIndex++;
+                if (interactableIndex >= nearbyInteractables.Count)
+                {
+                    interactableIndex = 0;
+                }
+                HighlightNearbyInteractable();
             }
         }
         #endregion
 
-            #region movement
-            inputMove = moveAction.ReadValue<Vector2>() * moveSpeed * Time.fixedDeltaTime;
+        #region movement
+        inputMove = moveAction.ReadValue<Vector2>() * moveSpeed * Time.fixedDeltaTime;
 
         //add vector to player position?
         //playerRB.AddForce(input_move * Time.fixedDeltaTime);
@@ -118,13 +140,15 @@ public class PlayerController : MonoBehaviour
             int tempID = other.gameObject.GetInstanceID();
             if (!nearbyInteractables.ContainsKey(tempID))
             {
+                //TODO: sort as it's added? sort by x position?
                 nearbyInteractables.Add(other.gameObject.GetInstanceID(), other.gameObject);
                 nearInteractable = true;
                 Debug.Log($"[PlayerController]: detected nearby interactable. ID: {other.gameObject.GetInstanceID()}");
-                if(nearbyInteractables.Count == 1)
-                {   
+                if (nearbyInteractables.Count == 1)
+                {
                     //Highlight nearest instead? Do we still check if it's the first one?
-                    HighlightNearbyInteractable(0);
+                    interactableIndex = 0;
+                    HighlightNearbyInteractable();
                 }
             }
         }
@@ -136,23 +160,61 @@ public class PlayerController : MonoBehaviour
         if (checkTest != null)
         {
             int tempID = other.gameObject.GetInstanceID();
-            if(nearbyInteractables.ContainsKey(other.gameObject.GetInstanceID()))
+            if (nearbyInteractables.ContainsKey(tempID))
             {
                 nearbyInteractables.Remove(tempID);
-                Debug.Log($"[PlayerController]:Removed interactable from list. ID: {tempID}");
 
-                if(nearbyInteractables.Count == 0)
+
+
+                if (nearbyInteractables.Count == 0)
                 {
                     nearInteractable = false;
-                    Debug.Log($"[PlayerController]:No longer near interactables. nearInteractable: {nearInteractable}");
+                    interactableIndex = -1;
+                    Debug.Log($"[PlayerController]:Will no longer be near interactables. nearInteractable: {nearInteractable}");
+                    HighlightNearbyInteractable(false);
                 }
+                else
+                {
+                    //if interactable index no longer valid, change it
+                    if ( interactableIndex>=nearbyInteractables.Count )
+                    {
+                        interactableIndex++;
+                        if (interactableIndex >= nearbyInteractables.Count)
+                        {
+                            interactableIndex = 0;
+                        }
+                    }
+
+                    HighlightNearbyInteractable();
+                }
+                Debug.Log($"[PlayerController]:Removed interactable from list. ID: {tempID}");
             }
         }
     }
 
-    private void HighlightNearbyInteractable(int index)
+    /// <summary>
+    /// Highlight selected interactable by moving icon to said interactable and
+    /// enabling it. If nearInteractables is false, will function as if interactablesExist set to false
+    /// </summary>
+    /// <param name="interactablesExist"> if set to false, moves icon back to player and disable</param>
+    private void HighlightNearbyInteractable(bool interactablesExist = true)
     {
-        Debug.Log($"[PlayerController]: Highlighting interactable at: index: {index}; ID: {nearbyInteractables.ElementAt(index).Value}");
-        interactableIndex = index;
+        if (!interactablesExist || !nearInteractable )
+        {
+            //Return interact icon back to player and disable
+            interactIconObject.transform.SetParent(gameObject.transform, false);
+            interactIconObject.SetActive(false);            
+        }
+        else if(interactableIndex >= nearbyInteractables.Count)
+        {
+            //This must not happen
+            Debug.LogError($"[PlayerController]: Attempted to highlighting interactable at: index: {interactableIndex} when nearbyInteractables only has {nearbyInteractables.Count} entries");
+        }
+        else
+        {
+            interactIconObject.SetActive(true);
+            interactIconObject.transform.SetParent(nearbyInteractables.ElementAt(interactableIndex).Value.transform, false);
+            Debug.Log($"[PlayerController]: Highlighting interactable at: index: {interactableIndex}; ID: {nearbyInteractables.ElementAt(interactableIndex).Key}");
+        }
     }
 }
