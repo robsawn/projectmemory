6de619e0154c9b033513232be905f1834f391da5
Niels Kamp <niels.kamp@xs4all.nl>
2025-12-18 22:26:12 +0100

    Moved Script + Loading Test Scene

--- projectmemory/Assets/Scripts/MainMenu.cs
+++ projectmemory/Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("SampleScene");
+        SceneManager.LoadSceneAsync("SingTestScene");
     }
 
     public void QuitGame()
